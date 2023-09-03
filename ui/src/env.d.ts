c388ec7405fab029a013f6272c2c3dbaad8a51fd
Alexander McLeod <alexwillmcleod@gmail.com>
2023-09-03 14:19:03 +1200

    feat: started changing to new design

--- ui/src/env.d.ts
+++ ui/src/env.d.ts
@@ -1,1 +1,4 @@
 /// <reference types="astro/client" />
+interface ImportMetaEnv {
+  readonly STRAPI_URL: string;
+}
--- ui/tailwind.config.cjs
+++ ui/tailwind.config.cjs
@@ -3,6 +3,7 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		fontFamily: {
+      'display': ['Omnes'],
       'sans': ['Atkinson Hyperlegible']
     },
 	},
@@ -15,7 +16,7 @@ module.exports = {
           "primary": "#1B1B25",
           "base-200": "#39A8C1",
           "base-100": "#BFE5EE",
-          "accent": "#19697B"
+          "accent": "#96CDA9"
         }
       }
     ]
