0d79954f09fdeae4107704dfd7fe5d306f8dc691
Alexander McLeod <alexwillmcleod@gmail.com>
2023-10-20 21:59:17 +1300

    added hi kalana

--- ui/src/components/calc/calculator.tsx
+++ ui/src/components/calc/calculator.tsx
@@ -5,6 +5,7 @@ export default function Calculator() {
     <div>
       <div class="w-full h-96 bg-white/40 rounded-xl p-4">
         <p class="w-full">Solid JS</p>
+        <p>Hi Kalana!</p>
       </div>
     </div>
   );
