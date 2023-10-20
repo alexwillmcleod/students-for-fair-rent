b9bddce459b73d8b911cbc454b9f5304f71b78f2
Alexander McLeod <alexwillmcleod@gmail.com>
2023-10-20 21:56:58 +1300

    feat: started calculator component

---
+++ ui/src/components/calc/calculator.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Calculator() {
+  return (
+    <div>
+      <div class="w-full h-96 bg-white/40 rounded-xl p-4">
+        <p class="w-full">Solid JS</p>
+      </div>
+    </div>
+  );
+}
