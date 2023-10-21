c39d27bbe157b66cc4535fc0323def38014e1dfc
Alexander McLeod <alexwillmcleod@gmail.com>
2023-10-21 20:02:03 +1300

    question changes

--- ui/src/components/calc/SelectYear.tsx
+++ ui/src/components/calc/SelectYear.tsx
@@ -14,14 +14,14 @@ export default function SelectYear(props: SelectYearProps) {
 
   return (
     <div class="flex flex-col justify-center items-center gap-8 p-10">
-      <p class="font-bold text-5xl">What Year will you be in 2024?</p>
+      <p class="font-bold text-5xl">Will 2024 be you First Year?</p>
       <div class="flex flex-col gap-4">
         <span class="flex flex-row gap-4 items-center">
-          <label class="text-xl" for='first-year'>First Year</label>
+          <label class="text-xl" for='first-year'>Yes</label>
           <input onChange={handleSelectYear} class="radio radio-primary" type='radio' id='first-year' name='year' value='first' checked={props.isFirstYear == true}/>
         </span>
         <span class="flex flex-row gap-4 items-center">
-          <label class="text-xl" for='second-year-plus'>Second Year +</label>
+          <label class="text-xl" for='second-year-plus'>No</label>
           <input onChange={handleSelectYear} class="radio radio-primary" type='radio' id='second-year-plus' name='year' value='later' checked={props.isFirstYear == false}/>
         </span>
       </div>
--- ui/src/components/calc/StudentFinance.tsx
+++ ui/src/components/calc/StudentFinance.tsx
@@ -38,20 +38,17 @@ export default function StudentFinance(props: StudentFinanceProps) {
       <p class="font-bold text-5xl">How much StudyLink assistance do you get?</p>
       <div class="flex flex-col gap-4">
         <span class="flex flex-col gap-4 items-center">
-          <p>How much Student Allowance do you receive</p>
+          <p>How much weekly Student Allowance do you receive for living costs?</p>
           <div class="flex flex-row gap-4">
-            <p>None</p>
-            <input type="range" min={0} max={300} value={props.weeklyAllowanceIncome} class="range range-md md:w-96" onChange={handleChangeStudentAllowance}/> 
-            <p>Maximum</p>
+            <input type="range" min={0} max={300} value={props.weeklyAllowanceIncome} class="range range-md range-success w-64 md:w-96" onChange={handleChangeStudentAllowance}/> 
+            <p>${props.weeklyAllowanceIncome}</p>
           </div>
         </span>
         <span class="flex flex-col gap-4 items-center">
-          <p>How much Student Loan do you take out</p>
-          <p>{}</p>
+          <p>How much weekly Student Loan do you take out for living costs?</p>
           <div class="flex flex-row gap-4">
-            <p>None</p>
-            <input type="range" min={0} max={300} value={props.weeklyLoanIncome} class="range range-md md:w-96" onChange={handleChangeStudentLoan}/> 
-            <p>Maximum</p>
+            <input type="range" min={0} max={300} value={props.weeklyLoanIncome} class="range range-md range-error w-64 md:w-96" onChange={handleChangeStudentLoan}/> 
+            <p>${props.weeklyLoanIncome}</p>
           </div>
         </span>
       </div>
