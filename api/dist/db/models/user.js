82793a9563c3ca8fffe61a1e112b48ea30e964e4
alexmcleod <alexwillmcleod@gmail.com>
2023-12-10 21:34:03 +1300

    refactor: removed dist folder

--- api/dist/db/models/user.js
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from 'mongoose';
-const userSchema = new Schema({
-    firstName: String,
-    lastName: String,
-    emailAddress: String,
-    upi: String,
-    studentId: String,
-});
-export const User = model('User', userSchema);
