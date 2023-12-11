9264077be6ffc531390a636b66b1e7e578e7e1d9
alexmcleod <alexwillmcleod@gmail.com>
2023-12-11 14:45:11 +1300

    feat: added netlify adapter

--- ui/astro.config.mjs
+++ ui/astro.config.mjs
@@ -3,11 +3,14 @@ import tailwind from '@astrojs/tailwind';
 import solidJs from '@astrojs/solid-js';
 import mdx from '@astrojs/mdx';
 
+import netlify from "@astrojs/netlify/functions";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), solidJs(), mdx()],
   prefetch: {
-    prefetchAll: true,
+    prefetchAll: true
   },
   output: 'server',
+  adapter: netlify()
 });
